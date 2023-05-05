07cae45626f49d13e0a189f38414eb59363e5834
Chan Weng Ho <cwengho@yahoo.com>
2023-05-05 20:43:56 +0800

    initial configuration from spring initializr

---
+++ src/main/java/com/tasik/projects/myfitnesspal/clone/MyfitnesspalCloneApplication.java
@@ -0,0 +1,13 @@
+package com.tasik.projects.myfitnesspal.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfitnesspalCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfitnesspalCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasik/projects/myfitnesspal/clone/MyfitnesspalCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasik.projects.myfitnesspal.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfitnesspalCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
